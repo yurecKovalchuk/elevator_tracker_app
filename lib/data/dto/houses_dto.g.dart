080a3b23bb59b78a1247a0cb5a58fa55fefe40e1
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-11-30 18:16:04 +0200

    feat: add houses_dto

---
+++ lib/data/dto/houses_dto.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'houses_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$HousesDTOImpl _$$HousesDTOImplFromJson(Map<String, dynamic> json) =>
+    _$HousesDTOImpl(
+      houseName: json['houseName'] as String?,
+      floors: json['floors'] as int?,
+      currentLiftPosition: json['currentLiftPosition'] as int?,
+    );
+
+Map<String, dynamic> _$$HousesDTOImplToJson(_$HousesDTOImpl instance) =>
+    <String, dynamic>{
+      'houseName': instance.houseName,
+      'floors': instance.floors,
+      'currentLiftPosition': instance.currentLiftPosition,
+    };
